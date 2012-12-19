c7ca1c1f6692f79dd72127debdc197b196c35928
Ye Chuah <yechuah@gmail.com>
2012-12-19 16:19:32 +1100

    property page

--- property.tpl.php
+++ property.tpl.php
@@ -1,1 +1,27 @@
-hello world
+<?php
+/**
+ * property template
+ * $title = Property title
+ * $chart = pie chart
+ */
+?>
+        <!-- Col 2 -->
+        <div id="content-col2" >
+
+<div class="centretitle"><?php print $title ?></div>
+<img src="" alt="" width="442px" height="240px">
+<div class="overview">
+<strong>Overview</strong>
+<p>
+<?php print $centre_narrative; ?>
+</p>
+</div>
+        </div>
+        <!-- // Col 2 -->
+        
+        <!-- Col 3 -->
+        <div id="content-col3" >
+<div class="chartitle"> Centre Composition by GLA </div>
+<?php print $chart ?>
+        </div>
+        <!-- // Col 3 -->
