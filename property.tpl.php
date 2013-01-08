e1d5607e0fca0e8825b3923d4200e85eea643815
Ye Chuah <yechuah@gmail.com>
2013-01-08 12:48:09 +1100

    pc continuing

--- property.tpl.php
+++ property.tpl.php
@@ -5,8 +5,9 @@
  * $chart = pie chart
  */
 ?>
-        <!-- Col 2 -->
-        <div id="content-col2" >
+
+<!-- Col 2 -->
+<div id="content-col2" >
 
 <div class="centretitle"><?php print $title ?></div>
 <img src="http://lorempixel.com/442/240/city" alt="" width="442px" height="240px">
@@ -23,10 +24,10 @@
         </div>
         <!-- // Col 2 -->
         
-        <!-- Col 3 -->
-        <div id="content-col3" style="margin-right:0">
+<!-- Col 3 -->
+<div id="content-col3">
 <div class="charttitle"> Centre Composition by GLA </div>
 <?php print $chart ?>
-        </div>
-        <!-- // Col 3 -->
+</div>
+<!-- // Col 3 -->
 
